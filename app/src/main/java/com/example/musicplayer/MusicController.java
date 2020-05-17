6e3e836df50e0d7b0b8c478f033b4f480fc0772b
DwiRizq <dwirizqi20@gmail.com>
2020-05-17 23:28:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/musicplayer/MusicController.java
@@ -0,0 +1,30 @@
+package com.example.musicplayer;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.MediaController;
+
+public class MusicController {
+    public MusicController(Context c) {
+        super();
+    }
+
+    public void show(){
+
+    }
+
+    public void hide() {
+    }
+
+    public void setPrevNextListeners(View.OnClickListener onClickListener, View.OnClickListener onClickListener1) {
+    }
+
+    public void setMediaPlayer(MainActivity mainActivity) {
+    }
+
+    public void setAnchorView(View viewById) {
+    }
+
+    public void setEnabled(boolean b) {
+    }
+}
